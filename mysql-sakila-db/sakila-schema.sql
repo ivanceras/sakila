c5e18fbf3c9e1024998962a9b181ad2dad0b73e6
Ruhid <38219617+Rruhid@users.noreply.github.com>
2023-09-03 13:45:44 +0400

    Update sakila-schema.sql

--- mysql-sakila-db/sakila-schema.sql
+++ mysql-sakila-db/sakila-schema.sql
@@ -1,29 +1,4 @@
--- Sakila Sample Database Schema
--- Version 0.8
-
--- Copyright (c) 2006, MySQL AB
--- All rights reserved.
-
--- Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
-
---  * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
---  * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
---  * Neither the name of MySQL AB nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
-
--- THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
-
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL';
-
-DROP SCHEMA IF EXISTS sakila;
-CREATE SCHEMA sakila;
-USE sakila;
-
---
--- Table structure for table `actor`
---
+ 
 
 CREATE TABLE actor (
   actor_id SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
@@ -88,27 +63,25 @@ CREATE TABLE country (
   PRIMARY KEY  (country_id)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+
+
+
 --
 -- Table structure for table `customer`
---
-
+-- 
 CREATE TABLE customer (
-  customer_id SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
-  store_id TINYINT UNSIGNED NOT NULL,
+  customer_id SERIAL PRIMARY KEY,
+  store_id SMALLINT NOT NULL,
   first_name VARCHAR(45) NOT NULL,
   last_name VARCHAR(45) NOT NULL,
-  email VARCHAR(50) DEFAULT NULL,
-  address_id SMALLINT UNSIGNED NOT NULL,
+  email VARCHAR(50),
+  address_id SMALLINT NOT NULL,
   active BOOLEAN NOT NULL DEFAULT TRUE,
-  create_date DATETIME NOT NULL,
-  last_update TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY  (customer_id),
-  KEY idx_fk_store_id (store_id),
-  KEY idx_fk_address_id (address_id),
-  KEY idx_last_name (last_name),
-  CONSTRAINT fk_customer_address FOREIGN KEY (address_id) REFERENCES address (address_id) ON DELETE RESTRICT ON UPDATE CASCADE,
-  CONSTRAINT fk_customer_store FOREIGN KEY (store_id) REFERENCES store (store_id) ON DELETE RESTRICT ON UPDATE CASCADE
-)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  create_date TIMESTAMP NOT NULL,
+  last_update TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
+);
+
+
 
 --
 -- Table structure for table `film`
@@ -236,20 +209,16 @@ CREATE TABLE language (
 --
 
 CREATE TABLE payment (
-  payment_id SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
-  customer_id SMALLINT UNSIGNED NOT NULL,
-  staff_id TINYINT UNSIGNED NOT NULL,
-  rental_id INT DEFAULT NULL,
-  amount DECIMAL(5,2) NOT NULL,
-  payment_date DATETIME NOT NULL,
-  last_update TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY  (payment_id),
-  KEY idx_fk_staff_id (staff_id),
-  KEY idx_fk_customer_id (customer_id),
-  CONSTRAINT fk_payment_rental FOREIGN KEY (rental_id) REFERENCES rental (rental_id) ON DELETE SET NULL ON UPDATE CASCADE,
-  CONSTRAINT fk_payment_customer FOREIGN KEY (customer_id) REFERENCES customer (customer_id) ON DELETE RESTRICT ON UPDATE CASCADE,
-  CONSTRAINT fk_payment_staff FOREIGN KEY (staff_id) REFERENCES staff (staff_id) ON DELETE RESTRICT ON UPDATE CASCADE
-)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  payment_id SERIAL PRIMARY KEY,
+  customer_id SMALLINT NOT NULL,
+  staff_id SMALLINT NOT NULL,
+  rental_id INT,
+  amount NUMERIC(5, 2) NOT NULL,
+  payment_date TIMESTAMP NOT NULL,
+  last_update TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
+);
+
+
 
 
 --
